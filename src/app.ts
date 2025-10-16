204cc508cc9d436871e5494f646174c355404958
unknown <you@example.com>
2025-10-16 18:14:09 +0600

    working on deploying

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,22 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import notFound from './app/middlewares/notFound.js';
 import globalErrorHandler from './app/middlewares/globalErrorHandler.js';
-import router from './app/routes/index.js';
 import cookieParser from 'cookie-parser';
+import router from './app/routes/index.js';
+import { BikeControllers } from './app/modules/bike/bike.controller.js';
 const app: Application = express();
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
 // ✅ Use cookie-parser middleware
 app.use(cookieParser());
 app.get('/', async (req: Request, res: Response) => {
   res.send('Hello World!');
 });
 
-async function func2(req: Request, res: Response) {
-  res.send('Hello World 2!');
-}
-
-app.get('/d', func2);
-
-// student routes 👇
+app.use('/api/bikes', BikeControllers.getAllBikes);
 app.use('/api', router);
 
 app.use(notFound);
